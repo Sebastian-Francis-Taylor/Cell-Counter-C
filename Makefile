114424baad3b373de06e2ebce6fef37de30cb006
Sebastian-Francis-Taylor <me@sebastian-taylor.com>
2025-09-26 12:32:33 +0200

    execution time analysis added

--- Makefile
+++ Makefile
@@ -1,19 +1,20 @@
 CC = gcc
 CFLAGS = -Wall -O3 -I./src
 DEBUG_CFLAGS = -Wall -g -O0 -DDEBUG -I./src
-
+TIMING_CFLAGS = -Wall -O3 -DTIMING -I./src
 SRC_DIR = src
 BUILD_DIR = build
 BIN_DIR = bin
-
 SRCS = $(SRC_DIR)/main.c $(SRC_DIR)/cbmp.c
 OBJS = $(SRCS:$(SRC_DIR)/%.c=$(BUILD_DIR)/%.o)
 DEBUG_OBJS = $(SRCS:$(SRC_DIR)/%.c=$(BUILD_DIR)/%_debug.o)
+TIMING_OBJS = $(SRCS:$(SRC_DIR)/%.c=$(BUILD_DIR)/%_timing.o)
 TARGET = $(BIN_DIR)/cell-counter
 TARGET_EXE = $(BIN_DIR)/cell-counter.exe
 DEBUG_TARGET = $(BIN_DIR)/cell-counter-debug
+TIMING_TARGET = $(BIN_DIR)/cell-counter-timing
 
-.PHONY: all debug clean valgrind
+.PHONY: all debug timing clean valgrind
 
 all: $(TARGET)
 
@@ -31,6 +32,12 @@ $(DEBUG_TARGET): $(DEBUG_OBJS)
 	@mkdir -p $(BIN_DIR)
 	$(CC) $(DEBUG_CFLAGS) -o $@ $^
 
+timing: $(TIMING_TARGET)
+
+$(TIMING_TARGET): $(TIMING_OBJS)
+	@mkdir -p $(BIN_DIR)
+	$(CC) $(TIMING_CFLAGS) -o $@ $^
+
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.c
 	@mkdir -p $(BUILD_DIR)
 	$(CC) $(CFLAGS) -c $< -o $@
@@ -39,8 +46,12 @@ $(BUILD_DIR)/%_debug.o: $(SRC_DIR)/%.c
 	@mkdir -p $(BUILD_DIR)
 	$(CC) $(DEBUG_CFLAGS) -c $< -o $@
 
+$(BUILD_DIR)/%_timing.o: $(SRC_DIR)/%.c
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(TIMING_CFLAGS) -c $< -o $@
+
 valgrind: debug
 	valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all $(DEBUG_TARGET)
 
 clean:
-	rm -rf $(BUILD_DIR)/*.o $(BUILD_DIR)/*_debug.o $(TARGET) $(TARGET_EXE) $(DEBUG_TARGET)
+	rm -rf $(BUILD_DIR)/*.o $(BUILD_DIR)/*_debug.o $(BUILD_DIR)/*_timing.o $(TARGET) $(TARGET_EXE) $(DEBUG_TARGET) $(TIMING_TARGET)
--- src/main.c
+++ src/main.c
@@ -17,8 +17,20 @@ const int PATTERN[3][3] = {{0, 1, 0}, {1, 1, 1}, {0, 1, 0}};
 #define SEARCH_WINDOW 14
 #define BINARY_THRESHOLD 127
 
-// change to theoretical max for cells in a 950 x 950 image
 #define MAX_COORDINATES 4700
+
+#ifdef TIMING
+#define START_TIMER() clock_t timer_start = clock()
+#define END_TIMER(label)                                                                                                                             \
+    do {                                                                                                                                             \
+        clock_t timer_end = clock();                                                                                                                 \
+        double cpu_time = ((double)(timer_end - timer_start)) / CLOCKS_PER_SEC;                                                                      \
+        printf("%s took %f seconds\n", label, cpu_time);                                                                                             \
+    } while (0)
+#else
+#define START_TIMER()
+#define END_TIMER(label)
+#endif
 
 typedef struct {
     int x;
@@ -44,6 +56,7 @@ static unsigned char greyscale_image[BMP_WIDTH][BMP_HEIGHT];
 static unsigned char binary_image[BMP_WIDTH][BMP_HEIGHT];
 
 void save_greyscale_image(unsigned char image[BMP_WIDTH][BMP_HEIGHT], char *save_path) {
+    START_TIMER();
     for (int x = 0; x < BMP_WIDTH; ++x) {
         for (int y = 0; y < BMP_HEIGHT; ++y) {
             for (int z = 0; z < BMP_CHANNELS; ++z) {
@@ -53,19 +66,23 @@ void save_greyscale_image(unsigned char image[BMP_WIDTH][BMP_HEIGHT], char *save
     }
 
     write_bitmap(output_image, save_path);
+    END_TIMER("save_greyscale_image");
 }
 
 // Applying the threshold on all pixels
 static void apply_threshold(unsigned int threshold, unsigned char input_image[BMP_WIDTH][BMP_HEIGHT],
                             unsigned char output_image[BMP_WIDTH][BMP_HEIGHT]) {
+    START_TIMER();
     for (int x = 0; x < BMP_WIDTH; ++x) {
         for (int y = 0; y < BMP_HEIGHT; ++y) {
             output_image[x][y] = (input_image[x][y] <= threshold) ? 0 : 255;
         }
     }
+    END_TIMER("apply_threshold");
 }
 
 int erode_image(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT], unsigned char output_image[BMP_WIDTH][BMP_HEIGHT]) {
+    START_TIMER();
     int eroded_any = 0; // Track if any pixel was eroded
 
     // Copy input image to output as default
@@ -101,11 +118,13 @@ int erode_image(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT], unsigned char 
             }
         }
     }
+    END_TIMER("erode_image");
     // return 1 if any pixel was eroded, 0 otherwise
     return eroded_any;
 }
 
 void greyscale_bitmap(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS]) {
+    START_TIMER();
     for (int x = 0; x < BMP_WIDTH; ++x) {
         for (int y = 0; y < BMP_HEIGHT; ++y) {
             unsigned int channel_sum = 0;
@@ -115,6 +134,7 @@ void greyscale_bitmap(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANN
             greyscale_image[x][y] = channel_sum / BMP_CHANNELS;
         }
     }
+    END_TIMER("greyscale_bitmap");
 }
 
 int has_white_pixel(unsigned char image[BMP_WIDTH][BMP_HEIGHT], int start_x, int start_y) {
@@ -152,6 +172,7 @@ void remove_spot(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT], int start_x, 
 }
 
 int detect_spots(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT]) {
+    START_TIMER();
     int cells_found = 0;
     // Stops the search before the array is out of bounds
     for (int x = 0; x <= BMP_WIDTH - SEARCH_WINDOW; x++) {
@@ -163,10 +184,12 @@ int detect_spots(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT]) {
             }
         }
     }
+    END_TIMER("detect_spots");
     return cells_found;
 }
 
 void generate_output_image(unsigned char image[BMP_WIDTH][BMP_HEIGHT]) {
+    START_TIMER();
     for (int x = 0; x < BMP_WIDTH; ++x) {
         for (int y = 0; y < BMP_HEIGHT; ++y) {
             for (int z = 0; z < BMP_CHANNELS; ++z) {
@@ -189,6 +212,7 @@ void generate_output_image(unsigned char image[BMP_WIDTH][BMP_HEIGHT]) {
             output_image[cx][cy + j][0] = 255; // Red
         }
     }
+    END_TIMER("generate_output_image");
 }
 
 int main(int argc, char **argv) {
@@ -207,6 +231,7 @@ int main(int argc, char **argv) {
 
     // Load image from file
     read_bitmap(argv[1], input_image);
+
     write_bitmap(input_image, "step_0.bmp");
 
     greyscale_bitmap(input_image);
@@ -228,6 +253,7 @@ int main(int argc, char **argv) {
             binary_image_2[x][y] = binary_image[x][y];
         }
     }
+
     int total_cells = 0;
     for (int i = 1; i <= 10; ++i) {
         erode_image((i % 2 == 0 ? binary_image : binary_image_2), (i % 2 == 0 ? binary_image_2 : binary_image));
